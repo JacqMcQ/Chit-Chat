bde7d842e5f9603ee716e3ca4b6fe74d4b47738f
JacMcQ <jacqlyn.mcquade@icloud.com>
2024-09-15 18:06:37 -0600

    added thought feature and logic

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,7 @@
-const mongoose = require("mongoose");
+const { connect, connection } = require("mongoose");
 
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/socialnetwork",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-);
+const connectionString = "mongodb://127.0.0.1:27017/Chit-ChatDB";
 
-module.exports = mongoose.connection;
+connect(connectionString);
+
+module.exports = connection;
